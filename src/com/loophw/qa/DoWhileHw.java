b4a960444313d1964feaa9655f556af372a1bd38
Md Rahman <mdrahman@Mds-MacBook-Pro.local>
2020-01-18 22:05:22 -0500

    first commit

---
+++ src/com/loophw/qa/DoWhileHw.java
@@ -0,0 +1,25 @@
+package com.loophw.qa;
+
+public class DoWhileHw {
+    public static void main(String [] args){
+        // for example #1
+        // int a = 5;
+
+        //do{
+        //system.out.println(a);
+        //a++;
+
+
+        //}while {a<15};
+
+        int i = 25;
+        do {
+
+            System.out.println(i);
+        }while (i>=15);
+
+        i++;
+
+
+    }
+}
---
+++ src/com/loophw/qa/WhileLoopHw.java
@@ -0,0 +1,21 @@
+package com.loophw.qa;
+
+public class WhileLoopHw {
+
+    public static void main(String []args){
+        int k = 25;
+        while (k>15){
+            System.out.println(k);
+            k--;
+        }
+
+    }
+    public static void whileloop(){
+        int l = 3;
+        while (l<=7){
+            System.out.println(l);
+            l++;
+        }
+    }
+
+}
